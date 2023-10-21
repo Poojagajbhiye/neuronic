b521cc92fbd47ed3ea6cc8f8ba46cde3ff2de792
Pooja Gajbhiye <poojagajbhiye563@gmail.com>
2023-10-21 12:32:24 +0530

    Add Data, Business and Presentation. Set-up data layer and add BrainPointData model

---
+++ lib/business/brain_point_model.dart
@@ -0,0 +1,17 @@
+class BrainPointData {
+  final String name;
+  int? pulseRateInHz;
+  int? intensityInPercentage;
+
+  BrainPointData({
+    required this.name,
+    this.pulseRateInHz,
+    this.intensityInPercentage,
+  });
+
+  factory BrainPointData.fromJSON(Map<String, dynamic> json) => BrainPointData(
+        name: json['name'] as String,
+        pulseRateInHz: json['pulseRateInHz'] as int,
+        intensityInPercentage: json['intensityInPercentage'] as int,
+      );
+}
---
+++ lib/data/data_source.dart
@@ -0,0 +1,117 @@
+/// This data layer with interact with cloud to get data and update data.
+// import 'package:http/http.dart' as http;
+
+const List<Map<String, dynamic>> allBrainPointsData = [
+  {
+    'name': 'FP1',
+    'pulseRateInHz': 80,
+    'intensityInPercentage': 50,
+  },
+  {
+    'name': 'FP2',
+    'pulseRateInHz': 81,
+    'intensityInPercentage': 51,
+  },
+  {
+    'name': 'F7',
+    'pulseRateInHz': 82,
+    'intensityInPercentage': 52,
+  },
+  {
+    'name': 'F8',
+    'pulseRateInHz': 90,
+    'intensityInPercentage': 53,
+  },
+  {
+    'name': 'F3',
+    'pulseRateInHz': 85,
+    'intensityInPercentage': 55,
+  },
+  {
+    'name': 'FZ',
+    'pulseRateInHz': 86,
+    'intensityInPercentage': 51,
+  },
+  {
+    'name': 'F4',
+    'pulseRateInHz': 100,
+    'intensityInPercentage': 90,
+  },
+  {
+    'name': 'T3',
+    'pulseRateInHz': 180,
+    'intensityInPercentage': 150,
+  },
+  {
+    'name': 'C3',
+    'pulseRateInHz': null,
+    'intensityInPercentage': null,
+  },
+  {
+    'name': 'CZ',
+    'pulseRateInHz': 80,
+    'intensityInPercentage': 50,
+  },
+  {
+    'name': 'C4',
+    'pulseRateInHz': null,
+    'intensityInPercentage': null,
+  },
+  {
+    'name': 'T4',
+    'pulseRateInHz': 80,
+    'intensityInPercentage': 50,
+  },
+  {
+    'name': 'P3',
+    'pulseRateInHz': 80,
+    'intensityInPercentage': 50,
+  },
+  {
+    'name': 'PZ',
+    'pulseRateInHz': null,
+    'intensityInPercentage': 70,
+  },
+  {
+    'name': 'P4',
+    'pulseRateInHz': 80,
+    'intensityInPercentage': 50,
+  },
+  {
+    'name': 'T5',
+    'pulseRateInHz': 120,
+    'intensityInPercentage': null,
+  },
+  {
+    'name': 'T6',
+    'pulseRateInHz': 80,
+    'intensityInPercentage': 50,
+  },
+  {
+    'name': '01',
+    'pulseRateInHz': 80,
+    'intensityInPercentage': 50,
+  },
+  {
+    'name': '02',
+    'pulseRateInHz': 80,
+    'intensityInPercentage': 50,
+  },
+];
+
+Future<List<Map<String, dynamic>>> getData() {
+  return Future.delayed(const Duration(seconds: 2), () {
+    return allBrainPointsData;
+  });
+}
+
+Future<List<Map<String, dynamic>>> fetchBrainPoints() async {
+  // final response =
+  //     await http.get(Uri.parse('htttps://actual_url/place_holder'));
+
+  // if (response.statusCode == 200) {
+  return await getData();
+  // } else {
+  //throw Exception('Failed to get brain points data.');
+  // }
+}
