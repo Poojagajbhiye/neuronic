3844d8bfc52e9721921d93132a62cc2276b144f5
Pooja Gajbhiye <poojagajbhiye563@gmail.com>
2023-10-21 14:06:34 +0530

    Update Clinical Protocols UI

--- lib/presentation/clinical_protocol/clinical_protocol_page.dart
+++ lib/presentation/clinical_protocol/clinical_protocol_page.dart
@@ -5,6 +5,56 @@ class ClinicalProtocolPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('QEEG Points 1'),
+      ),
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              Container(
+                width: double.infinity,
+                height: 350.0,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/brain.png'),
+                  ),
+                ),
+              ),
+              const Positioned(
+                left: 140,
+                top: 80,
+                child: CircleAvatar(
+                  child: Text('FP1'),
+                ),
+              ),
+              const Positioned(
+                right: 140,
+                top: 80,
+                child: CircleAvatar(
+                  child: Text('FP2'),
+                ),
+              ),
+              const Positioned(
+                left: 60,
+                top: 110,
+                child: CircleAvatar(
+                  child: Text('F7'),
+                ),
+              ),
+              const Positioned(
+                right: 60,
+                top: 110,
+                child: CircleAvatar(
+                  child: Text('F8'),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
